037976f81d13119b7314a56402125babacf47ae3
Umutesi23 <setpascal27@gmail.com>
2024-05-02 16:06:21 +0200

    another m

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -45,7 +45,7 @@ const router = useRouter();
   async function onSubmit(data: z.infer<typeof formSchema>) {
 const singInData= await signIn("credentials", {email: data.email, password: data.password, redirect:true})
 //change pages
-singInData?.error? console.log(singInData.error): router.push("/tradeform")
+router.push("/tradeform")
 
 }
   
